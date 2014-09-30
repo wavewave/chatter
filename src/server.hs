71d1559a8c72b3d5fcb70c222a7025ad3cd376f9
Ian-Woo Kim <ianwookim@gmail.com>
2014-09-30 15:34:43 +0200

    messagequeue as a separate thread. then broadcaster works without blocking

--- src/server.hs
+++ src/server.hs
@@ -4,33 +4,43 @@
 import           Control.Concurrent
 import           Control.Concurrent.STM
 import           Control.Monad (forever)
+import           Control.Monad.Loops
 import           Network.Simple.TCP
 --
 import           Common
+
+messagequeue :: TVar [Message] -> TVar [Message] -> IO ()
+messagequeue tvar tvarLog = forever $ atomically $ do 
+    bufmsgs <-readTVar tvar
+    if null bufmsgs 
+      then retry 
+      else do 
+        writeTVar tvar []
+        logs <- readTVar tvarLog
+        let newlogs = bufmsgs ++ logs
+        writeTVar tvarLog newlogs 
 
 main :: IO ()
 main = do 
   putStrLn " I am server " 
   tvar <- atomically $ newTVar ([] :: [Message] )
   tvarLog <- atomically $ newTVar ([] :: [Message])
+  forkIO $ messagequeue tvar tvarLog
+
   forkIO $ serve HostAny "5002" $ \(sock,addr) -> do 
     putStrLn $ "TCP connection established from " ++ show addr
-    forever $ do 
-      nmsgs <- atomically $ do 
-                 bufmsgs <-readTVar tvar
-                 if null bufmsgs 
-                   then retry 
-                   else writeTVar tvar []
-                 return bufmsgs
-      newlogs <- atomically $ do 
-                   logs <- readTVar tvarLog
-                   let newlogs = nmsgs ++ logs
-                   writeTVar tvarLog newlogs 
-                   return newlogs
-      print newlogs
-      packAndSend sock newlogs -- (Message n "hello") 
+    let go n = do (n',logs) <- atomically $ do
+                                 logs <- readTVar tvarLog
+                                 let n' = length logs
+                                 if length logs == n 
+                                   then retry
+                                   else return (n',logs)
+                  print logs
+                  packAndSend sock logs 
+                  go n'
+    go 0
 
-  serve HostAny "5003" $ \(sock,addr) -> forever $ do 
+  serve HostAny "5003" $ \(sock,addr) -> unfoldM_ $ do 
     putStrLn $ "Getting message from " ++ show addr
     mtxt :: Maybe Message <- recvAndUnpack sock
     case mtxt of 
@@ -39,6 +49,7 @@ main = do
                      atomically $ do 
                        txts <- readTVar tvar
                        writeTVar tvar (txt:txts)
+    return mtxt
   -- 
   getLine
   return ()
